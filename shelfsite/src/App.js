e8a86c4eed561fed41bb1cbcb5cd4d407b16452e
Milen <milen@live.unc.edu>
2020-04-17 16:35:51 -0400

    Added basic styling to all components that have been created thus far

--- shelfsite/src/App.js
+++ shelfsite/src/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import StoreSearcherComponent from './components/StoreSearcherComponent.js' 
+import './styles/HomePageStyle.css'
+import Logo from "./include/shelfCheckLogoTransparent.png"
+
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<p> Test </p>
+			<div className="general">
+				<img src={Logo} alt="Logo" className="Logo"/>
+				<p> Welcome to shelfCheck! </p>
 			    <StoreSearcherComponent itemName="Bread" lat="35.82" lon="-78.77"/>
 			</div>
 		)
--- shelfsite/src/components/StoreComponents.js
+++ shelfsite/src/components/StoreComponents.js
@@ -1,21 +1,15 @@
 import React from "react"
+import '../styles/StoreComponentStyle.css'
 
 function StoreComponent(props) {
-	let textStyle = {color: "black"}
 	return (
-		<div>
-		<div style={{color: "blue", backgroundColor: "orange", textAlign: "center", display:"flex", flexDirection:"column"}}>
-		<p style={textStyle}>Store Name: {props.name}</p>
-		<p style={textStyle}>Store Address: {props.addy}</p>
-		<p style={textStyle}>Store Latitdue: {props.latitude}</p>
-		<p style={textStyle}>Store Longitude: {props.longitude}</p>
-		<p style={textStyle}>Store Quantity: {props.quantity}</p>
-		<p style={textStyle}>Store Distance: {props.distance}</p>
-		</div>
-		
-		<div>
-		<hr />
-		</div>
+		<div className='primary'>
+		<p> Store Name: {props.name}</p>
+		<p >Store Address: {props.addy}</p>
+		<p >Store Latitdue: {props.latitude}</p>
+		<p >Store Longitude: {props.longitude}</p>
+		<p >Store Quantity: {props.quantity}</p>
+		<p >Store Distance: {props.distance}</p>
 		</div>
 	)
 }
