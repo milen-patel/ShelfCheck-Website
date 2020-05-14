b8cac3b7db5b695a8defb567aa5885d97db5f3d7
Milen <milen@live.unc.edu>
2020-05-13 21:28:55 -0400

    Added Support For Browser Resizing

--- shelfsite/src/components/HomePageContent.js
+++ shelfsite/src/components/HomePageContent.js
@@ -11,9 +11,8 @@ class HomePageContent extends Component {
 		return (
 			<div className="main">
 				<h1> What We Do </h1>
-				<p>ShelfCheck is a novel data solutions company providing consumer-sourced reports of current store inventories. Inspired by the country-wide shortages of common household goods due to the the COVID-19 pandemic, our founders sought to develop a tool that could make one-stop shopping a reality.  </p>
+				<p className="companyExplanation">ShelfCheck is a novel data solutions company providing consumer-sourced reports of current store inventories. Inspired by the country-wide shortages of common household goods due to the the COVID-19 pandemic, our founders sought to develop a tool that could make one-stop shopping a reality.  </p>
 				<h1> Meet the Team </h1>
-
 
 			<div className="team">
 				<TeamPersonVisualizer name = "Anthony Guzzo" role = "Chief Design Officer, UX Specialist" imgloc="../include/AnthonyGuzzo.jpeg"/>
@@ -22,6 +21,7 @@ class HomePageContent extends Component {
 				<TeamPersonVisualizer name = "James Taylor" role = "Co-CTO, Head of App Platform Development" imgloc="../include/JamesTaylor.jpeg"/>
 				<TeamPersonVisualizer name = "Milen Patel" role = "Co-CTO, Head of Web Platform Development" imgloc="../include/MilenPatel.jpeg"/>
 			</div>
+
 
 			</div>
 		)
