c13f840aef25bc30d119622762607d82c7496031
Milen <milen@live.unc.edu>
2020-05-14 18:07:47 -0400

    Set up router

--- shelfsite/src/App.js
+++ shelfsite/src/App.js
@@ -2,13 +2,22 @@ import React, {Component} from 'react';
 import './styles/HomePageStyle.css'
 import NavBar from './components/Navbar.js'
 import HomePageContent from './components/HomePageContent.js'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 class App extends Component {
 	render() {
 		return (
+			<switch>
 			<div className="general">
-				<NavBar />
-				<HomePageContent />
+				<Route path="/" render={props =>
+					<div>
+						<NavBar />
+						<HomePageContent />
+					</div>
+				} exact />
+				<Route path="/test" component={NavBar, HomePageContent} />
 			</div>
+			</switch>
 		)
 	}
 
--- shelfsite/src/index.js
+++ shelfsite/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<BrowserRouter>
+		<React.StrictMode>
+		<App />
+		</React.StrictMode>
+	</BrowserRouter>,
   document.getElementById('root')
 );
 
