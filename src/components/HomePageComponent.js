adcb5fddcd6fc885bf69dff46f5a274b8931a7b7
Milen <milen@live.unc.edu>
2020-05-16 18:18:39 -0400

    Improved animations, added kofi

--- src/components/HomePageComponent.js
+++ src/components/HomePageComponent.js
@@ -18,6 +18,9 @@ function HomePage() {
 							<Link to="/contact">
 								<button type="button" className="ContactButton"> Contact Us</button>
 							</Link>
+							<Link to="coffee.html" target="_blank">
+								<button type="button" className="ContactButton"> Buy A Coffee</button>
+							</Link>
 						</div>
 					</div>
 				</BrowserView>
