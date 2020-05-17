9c80a92ceabe75335d1205ddbc77f775049b3bb9
Milen <milen@live.unc.edu>
2020-05-16 23:46:21 -0400

    Fixed checklist bug

--- src/components/ItemChooser.js
+++ src/components/ItemChooser.js
@@ -11,18 +11,14 @@ class ItemChooser  extends React.Component {
 	}
 
 	handleCategoryChange(event) {
-		console.log("Change Detected");
-		console.log(this.state.itemsSelected);
 		this.setState({
               categoryToShow: event.target.value 
         });
 	}
 
 	handleIndividualItemSelectChange(e) {
-		console.log("Change Detected");
 		var iName = e.target.id;
 		if (this.state.itemsSelected.includes(iName)) {
-			console.log("YES");
 			this.setState((state) => {
 				var index = state.itemsSelected.indexOf(iName);
 				if (index !== -1) {
@@ -31,7 +27,6 @@ class ItemChooser  extends React.Component {
 				}
 			});
 		} else {
-			console.log("NO");
 			this.setState((old) => {
 					old.itemsSelected.push(iName);
 					return ({itemsSelected: old.itemsSelected});
@@ -40,6 +35,9 @@ class ItemChooser  extends React.Component {
 	}
 
 	shouldBeChecked(name) {
+		if(this.state.itemsSelected.includes(name)) {
+			return true;
+		}
 		return false;
 	}
 
@@ -47,50 +45,50 @@ class ItemChooser  extends React.Component {
 		if (this.state.categoryToShow === "nutrition") {
 			return (
 				<form>
-					<input type="checkbox" id="bread" name="bread" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("bread")} />
+					<input type="checkbox" id="bread" name="bread" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("bread")} />
 					<label htmlFor="bread"> Bread </label>
-					<input type="checkbox" id="milk" name="milk" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("milk")} />
+					<input type="checkbox" id="milk" name="milk" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("milk")} />
 					<label htmlFor="milk"> Milk </label>
-					<input type="checkbox" id="eggs" name="eggs" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("eggs")} />
+					<input type="checkbox" id="eggs" name="eggs" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("eggs")} />
 					<label htmlFor="eggs"> Eggs </label>
-					<input type="checkbox" id="water" name="water" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("water")} />
+					<input type="checkbox" id="water" name="water" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("water")} />
 					<label htmlFor="water"> Water </label>
-					<input type="checkbox" id="beef" name="beef" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("beef")} />
+					<input type="checkbox" id="beef" name="beef" value="first" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("beef")} />
 					<label htmlFor="beef"> Ground Beef </label>
 				</form>
 			);
 		} else if (this.state.categoryToShow === "power") {
 			return (
 				<form>
-					<input type="checkbox" id="toilet_paper" name="toilet_paper" value="second" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("toilet_paper")} />
+					<input type="checkbox" id="toilet_paper" name="toilet_paper" value="second" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("toilet_paper")} />
 					<label htmlFor="toilet_paper"> Toilet Paper </label>
-					<input type="checkbox" id="diapers" name="diapers" value="second" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("diapers")} />
+					<input type="checkbox" id="diapers" name="diapers" value="second" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("diapers")} />
 					<label htmlFor="diapers"> Diapers </label>
-					<input type="checkbox" id="masks" name="masks" value="second" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("masks")} />
+					<input type="checkbox" id="masks" name="masks" value="second" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("masks")} />
 					<label htmlFor="masks"> Masks </label>
 				</form>
 			);
 		} else if (this.state.categoryToShow === "cleaning") {
 			return (
 				<form>
-					<input type="checkbox" id="garbage_bags" name="garbage_bags" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("garbage_bags")} />
+					<input type="checkbox" id="garbage_bags" name="garbage_bags" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("garbage_bags")} />
 					<label htmlFor="garbage_bags"> Garbage Bags </label>
-					<input type="checkbox" id="clorox_wipes" name="clorox_wipes" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("clorox_wipes")} />
+					<input type="checkbox" id="clorox_wipes" name="clorox_wipes" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("clorox_wipes")} />
 					<label htmlFor="clorox_wipes"> Clorox Wipes </label>
-					<input type="checkbox" id="sanitizer" name="sanitizer" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("sanitizer")} />
+					<input type="checkbox" id="sanitizer" name="sanitizer" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("sanitizer")} />
 					<label htmlFor="sanitizer"> Hand Sanitizer </label>
-					<input type="checkbox" id="hand_soap" name="hand_soap" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("hand_soap")} />
+					<input type="checkbox" id="hand_soap" name="hand_soap" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("hand_soap")} />
 					<label htmlFor="hand_soap"> Hand Soap </label>
-					<input type="checkbox" id="paper_towels" name="paper_towels" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("bread")} />
+					<input type="checkbox" id="paper_towels" name="paper_towels" value="third" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("bread")} />
 					<label htmlFor="paper_towels"> Paper Towels </label>
 				</form>
 			);
 		} else if (this.state.categoryToShow === "health") {
 			return (
 				<form>
-					<input type="checkbox" id="batteries" name="batteries" value="fourth" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("batteries")} />
+					<input type="checkbox" id="batteries" name="batteries" value="fourth" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("batteries")} />
 					<label htmlFor="batteries"> Batteries </label>
-					<input type="checkbox" id="flashlights" name="flashlights" value="fourth" onChange={this.handleIndividualItemSelectChange.bind(this)} defaultChecked={this.shouldBeChecked("flashlights")} />
+					<input type="checkbox" id="flashlights" name="flashlights" value="fourth" onChange={this.handleIndividualItemSelectChange.bind(this)} checked={this.shouldBeChecked("flashlights")} />
 					<label htmlFor="flashlights"> Flashlights </label>
 				</form>
 			);
